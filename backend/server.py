3bd97df079d57e57a3a182059d3f345d15a2a7de
Aydan Gooneratne <aydangooneratne@Aydans-MacBook-Pro.local>
2023-04-30 19:06:51 -0400

    formatting

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-'''
+"""
 Backend code. 
 
 1. Sets up flask server.
@@ -7,7 +7,7 @@ Backend code.
 
 The backend will store a pandas dateframe of Spotify authenticated users and the
 state of their pending/complete matches with other spotify authenticated users.
-'''
+"""
 from flask import Flask, request, jsonify
 import pandas as pd
 import hashlib
@@ -18,26 +18,38 @@ import string
 from flask_cors import CORS, cross_origin
 import spotify_client as sc
 
-user_data = pd.DataFrame(columns=["user_id", "access_token", "refresh_token", "user_data", "expires", "accepted_invites"])
+user_data = pd.DataFrame(
+    columns=[
+        "user_id",
+        "access_token",
+        "refresh_token",
+        "user_data",
+        "expires",
+        "accepted_invites",
+    ]
+)
 matches = pd.DataFrame(columns=["id", "host", "guest"])
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
+
 
 def hash(s):
-    salt_string = 'salt_string'.encode()
+    salt_string = "salt_string".encode()
     hash_object = hashlib.sha256(s.encode() + salt_string)
     hash_bytes = hash_object.digest()
     hash_b64 = base64.b64encode(hash_bytes)
-    hash_str = hash_b64.decode('utf-8')
+    hash_str = hash_b64.decode("utf-8")
     return hash_str[:16]
+
 
 def create_match_id():
     alphabet = string.ascii_letters + string.digits
-    random_string = ''.join(secrets.choice(alphabet) for _ in range(16))
+    random_string = "".join(secrets.choice(alphabet) for _ in range(16))
     return random_string
 
-@app.route('/', methods=["GET"])
+
+@app.route("/", methods=["GET"])
 def home_page():
     """
     Check if the user has authenticated with Spotify yet. If not, the frontend will
@@ -47,14 +59,14 @@ def home_page():
     Returns { authenticated: bool }
     """
     sid = request.args.get("sid")
-    if (sid == "null"):
-        return jsonify({ "authenticated": False })
-    user_exists = user_data['user_id'] == sid
+    if sid == "null":
+        return jsonify({"authenticated": False})
+    user_exists = user_data["user_id"] == sid
     user_exists = True if user_exists.any() else False
-    return jsonify({ "authenticated": user_exists })
+    return jsonify({"authenticated": user_exists})
 
 
-@app.route('/match_state/<string:match_id>', methods=["GET"])
+@app.route("/match_state/<string:match_id>", methods=["GET"])
 def match_state(match_id):
     """
     Checks the state of the match.
@@ -63,28 +75,29 @@ def match_state(match_id):
     global matches
     match = matches[matches["id"] == match_id]
     if match.empty:
-        return jsonify({ "state": "invalid", "reason": "no match with that id" })
+        return jsonify({"state": "invalid", "reason": "no match with that id"})
     sid = request.args.get("sid")
     if sid == "null":
-        return jsonify({ "state": "invalid", "reason": "no sid given" })
+        return jsonify({"state": "invalid", "reason": "no sid given"})
     host = match["host"].iloc[0]
     guest = match["guest"].iloc[0]
     is_host = host == sid
     if is_host and guest is None:
-        return jsonify({ "state": "waiting" })
+        return jsonify({"state": "waiting"})
     elif not is_host and guest is None:
-        matches.loc[matches['id'] == match_id, 'guest'] = sid
-        return jsonify({ "state": "generating" }) 
+        matches.loc[matches["id"] == match_id, "guest"] = sid
+        return jsonify({"state": "generating"})
     elif is_host and guest is not None:
-        return jsonify({ "state": "generating" }) 
-    return jsonify({ "state": "invalid", "reason":"no case matched"})
+        return jsonify({"state": "generating"})
+    return jsonify({"state": "invalid", "reason": "no case matched"})
 
-@app.route('/match/<string:match_id>', methods=["GET"])
+
+@app.route("/match/<string:match_id>", methods=["GET"])
 def match(match_id):
-    '''
+    """
     Returns the match data for the given match id.
-    '''
-    error = jsonify({ "error": "there was an error" })
+    """
+    error = jsonify({"error": "there was an error"})
     match = matches[matches["id"] == match_id]
     if match.empty:
         return error
@@ -93,38 +106,48 @@ def match(match_id):
     if guest is None or host is None:
         return error
     host_access_token = user_data[user_data["user_id"] == host]["access_token"].iloc[0]
-    guest_access_token = user_data[user_data["user_id"] == guest]["access_token"].iloc[0]
-    print('getting host data...')
+    guest_access_token = user_data[user_data["user_id"] == guest]["access_token"].iloc[
+        0
+    ]
+    print("getting host data...")
     host_data = sc.get_user_data(host_access_token)
-    print('getting guest data...')
+    print("getting guest data...")
     guest_data = sc.get_user_data(guest_access_token)
     comparison = host_data.compare_with(guest_data)
-    return jsonify({ "host": host_data.to_dict(), "guest": guest_data.to_dict(), "match": comparison} )
+    return jsonify(
+        {
+            "host": host_data.to_dict(),
+            "guest": guest_data.to_dict(),
+            "match": comparison,
+        }
+    )
 
-@app.route('/create_match', methods=["GET"])
+
+@app.route("/create_match", methods=["GET"])
 def create_match():
-    '''
+    """
     Creates a match with the given match id.
-    '''
+    """
     # generate random match id
     match_id = create_match_id()
     sid = request.args.get("sid")
     if sid == "null":
-        return jsonify({ "error": "no user id" })
+        return jsonify({"error": "no user id"})
     global matches
     match = matches[matches["id"] == match_id]
     if not match.empty:
-        return jsonify({ "error": "match id already exists" })
-    matches.loc[len(matches)] = { "id": match_id, "host": sid, "guest": None }
-    print('create match:', { "id": match_id, "host": sid, "guest": None })
-    return jsonify({ "match_id": match_id })
-        
+        return jsonify({"error": "match id already exists"})
+    matches.loc[len(matches)] = {"id": match_id, "host": sid, "guest": None}
+    print("create match:", {"id": match_id, "host": sid, "guest": None})
+    return jsonify({"match_id": match_id})
 
-@app.route('/connect_account', methods=["GET"])
+
+@app.route("/connect_account", methods=["GET"])
 def connect_account():
-    return jsonify({ "redirect_url": sc.get_connect_account_url() })
+    return jsonify({"redirect_url": sc.get_connect_account_url()})
 
-@app.route('/authorize', methods=["GET"])
+
+@app.route("/authorize", methods=["GET"])
 @cross_origin()
 def authorize():
     code = request.args.get("code")
@@ -134,13 +157,14 @@ def authorize():
         "access_token": data["access_token"],
         "refresh_token": data["refresh_token"],
         "user_data": None,
-        "expires": data["expires_in"] + int(time.time())
+        "expires": data["expires_in"] + int(time.time()),
     }
     global user_data
     user_data.loc[len(user_data)] = user
-    res = jsonify({ "sid": user["user_id"] })
-    return res 
+    res = jsonify({"sid": user["user_id"]})
+    return res
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(sc.CLIENT_ID, sc.CLIENT_SECRET)
     app.run(debug=True, port=8000)
--- backend/spotify_client.py
+++ backend/spotify_client.py
@@ -7,13 +7,14 @@ import urllib.parse
 import requests
 import os
 
-CLIENT_ID = '4d3f871c854b41d1ac57aa40321a98cf'
+CLIENT_ID = "4d3f871c854b41d1ac57aa40321a98cf"
 CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
-SCOPE = 'user-read-private user-read-email user-top-read'
-AUTHORIZE_URL = 'https://accounts.spotify.com/authorize'
-TOKEN_URL = 'https://accounts.spotify.com/api/token'
-BASE_API_URL = 'https://api.spotify.com/v1'
+SCOPE = "user-read-private user-read-email user-top-read"
+AUTHORIZE_URL = "https://accounts.spotify.com/authorize"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+BASE_API_URL = "https://api.spotify.com/v1"
 FRONTEND_URL = "http://localhost:3000/"
+
 
 def get_connect_account_url():
     """
@@ -26,12 +27,13 @@ def get_connect_account_url():
         "client_id": CLIENT_ID,
         "response_type": "code",
         "redirect_uri": FRONTEND_URL,
-        "scope": SCOPE
+        "scope": SCOPE,
     }
     query_string = urllib.parse.urlencode(params)
     return AUTHORIZE_URL + "?" + query_string
 
-def authorize(code):  
+
+def authorize(code):
     """
     Wrapper for all interactions with the spotify API.
     """
@@ -56,22 +58,21 @@ def authorize(code):
         "redirect_uri": FRONTEND_URL,
     }
     client_creds = f"{CLIENT_ID}:{CLIENT_SECRET}"
-    client_creds_b64 = base64.b64encode(client_creds.encode('utf-8'))
-    auth_header_value = client_creds_b64.decode('utf-8')
+    client_creds_b64 = base64.b64encode(client_creds.encode("utf-8"))
+    auth_header_value = client_creds_b64.decode("utf-8")
     headers = {
         "Authorization": f"Basic {auth_header_value}",
         "Content-Type": "application/x-www-form-urlencoded",
-        "Accept": "application/json"
+        "Accept": "application/json",
     }
     body = urllib.parse.urlencode(body_params)
     res = requests.post(TOKEN_URL, headers=headers, data=body)
-    if (res.status_code != 200):
-        print('Error:', res.text)
+    if res.status_code != 200:
+        print("Error:", res.text)
     return json.loads(res.text)
 
 
-
-def reauthorize(refresh_token: str):  
+def reauthorize(refresh_token: str):
     """
     Reauthorize a user through Spotify whose current access token is about to expire.
 
@@ -86,6 +87,7 @@ def reauthorize(refresh_token: str):
     """
     pass
 
+
 def get_user_data(access_token: str) -> UserData:
     """
     Make multiple requests to Spotify API to get all the listening information of a user.
@@ -95,25 +97,31 @@ def get_user_data(access_token: str) -> UserData:
 
     Returns: UserData object which contains all the user's relevant listening information.
     """
-    
+
     # profile info
-    headers = { 'Authorization': f'Bearer {access_token}' }
-    res = requests.get(BASE_API_URL + '/me', headers=headers)
+    headers = {"Authorization": f"Bearer {access_token}"}
+    res = requests.get(BASE_API_URL + "/me", headers=headers)
     data = json.loads(res.text)
     user = UserData(data)
 
     # top artists
-    time.sleep(1) # rate limit
-    res = requests.get(BASE_API_URL + '/me/top/artists?time_range=medium_term&limit=50&offset=0', headers=headers)
+    time.sleep(1)  # rate limit
+    res = requests.get(
+        BASE_API_URL + "/me/top/artists?time_range=medium_term&limit=50&offset=0",
+        headers=headers,
+    )
     data = json.loads(res.text)
     user.set_top_artists(data["items"])
     print(user.top_artists)
     # top genres
-    time.sleep(1) # rate limit
+    time.sleep(1)  # rate limit
     user.set_top_genres(data["items"])
     print(user.top_genres)
     # top tracks
-    res = requests.get(BASE_API_URL + '/me/top/tracks?time_range=medium_term&limit=50&offset=0', headers=headers)
+    res = requests.get(
+        BASE_API_URL + "/me/top/tracks?time_range=medium_term&limit=50&offset=0",
+        headers=headers,
+    )
     data = json.loads(res.text)
     user.set_top_tracks(data["items"])
     print(user.top_tracks)
